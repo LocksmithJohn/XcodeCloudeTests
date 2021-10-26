98c4336e1db5f122c1ec3652775f0009babbde8a
LocksmithJohn <slusarzjan@gmail.com>
2021-10-26 11:40:45 +0200

    first project launch

---
+++ XcodeCloudeTests/XcodeCloudeTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudeTestsApp.swift
+//  XcodeCloudeTests
+//
+//  Created by Jan Slusarz on 26/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudeTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
